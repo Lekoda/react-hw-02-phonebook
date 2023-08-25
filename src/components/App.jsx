364a5590d21260ff72518999dab37d03ada9b8ab
Valeria Otchenash <kratorchestra@gmail.com>
2023-08-25 16:37:21 +0300

    adds contactForm comp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-
+import { ContactForm } from './ContactForm/ContactForm';
 export class App extends Component {
   state = {
     contacts: [],
     name: '',
   };
+
+  addContact = newContact => {
+    this.setState(prevState => {
+      return { contacts: [...prevState.contacts, newContact] };
+    });
+  };
   render() {
     return (
       <div>
         <h1>Phonebook</h1>
+        <ContactForm onAdd={this.addContact} />
         <h2>Contacts</h2>
       </div>
     );
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,31 +1,45 @@
-import { Formik } from 'formik';
-
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { nanoid } from 'nanoid';
 import * as Yup from 'yup';
 
-export const ContactForm = () => {
+const phoneRegistr =
+  /^\+?\d{1,4}?[ .-]?(\(\d{1,3}\))?([ .-]?\d{1,4}){1,4}([ .-]?\d{1,9})?$/;
+const addContactSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, 'Too short!')
+    .max(70, 'Too Long!')
+    .required('Required'),
+  number: Yup.string()
+    .matches(phoneRegistr, 'Phone number is not valid')
+    .required('Required'),
+});
+export const ContactForm = ({ onAdd }) => {
   return (
     <Formik
-      initialValues={{ name: '', number: '' }}
-      onSubmit={values => {
+      initialValues={{
+        name: '',
+        number: '',
+      }}
+      validationSchema={addContactSchema}
+      onSubmit={(values, action) => {
         console.log(values);
+        onAdd({ ...values, id: nanoid() });
+        action.resetForm();
       }}
     >
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          onChange={handleChange}
-          value={values.name}
-        />
-
-        <input
-          type="tel"
-          name="number"
-          onChange={handleChange}
-          value={values.number}
-        />
-        <button type="submit">Add contact</button>
-      </form>
+      <Form>
+        <label>
+          Name
+          <Field type="text" name="name" />
+          <ErrorMessage name="name" />
+        </label>
+        <label>
+          Number
+          <Field type="tel" name="number" />
+          <ErrorMessage name="number" />
+        </label>
+        <button type="submit">Submit</button>
+      </Form>
     </Formik>
   );
 };
